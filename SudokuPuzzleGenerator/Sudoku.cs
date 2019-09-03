2f85e95c1c854bb12ed112990d2808404e6a2e0d
BryanHo10 <bryankho1010@gmail.com>
2019-09-03 12:40:34 -0700

    Initialize Sudoku Class

---
+++ SudokuPuzzleGenerator/Sudoku.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SudokuPuzzleGenerator
+{
+    public class Sudoku
+    {
+        private int RowSize,ColSize;
+        public List<List<int>> PuzzleGrid;
+        
+        public Sudoku()
+        {
+            RowSize = ColSize = 9;
+            PuzzleGrid = new List<List<int>>();
+        }
+        public void printPuzzle()
+        {
+            for(int Column_Position = 0; Column_Position < ColSize; Column_Position++)
+            {
+                foreach(List<int> cellVal in PuzzleGrid)
+                {
+                    Console.Write($" {cellVal[Column_Position]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public 
+
+    }
+}
